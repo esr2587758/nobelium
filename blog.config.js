e12f6f5e9c97c312df6b1d43e6ef1c207bbd41db
人生如戏全靠演技 <esr2587758@gmail.com>
2023-11-09 10:46:38 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -33,7 +33,7 @@ const BLOG = {
       domainId: '' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
     },
     gaConfig: {
-      measurementId: 'GTM-NLK9BXKT' // e.g: G-XXXXXXXXXX
+      measurementId: 'G-9CGEN7G7GJ' // e.g: G-XXXXXXXXXX
     }
   },
   comment: {
